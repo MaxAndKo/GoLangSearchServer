80d7e38b21b0f571d988cf03baf0ef872f330759
maxandko <maksim@konchalov.ru>
2025-07-03 20:41:56 +0400

    Первый общий тест

--- client_test.go
+++ client_test.go
@@ -1,3 +1,37 @@
 package main
 
-// код писать тут
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	server := startSearchServer()
+
+	searchClient := SearchClient{
+		AccessToken: "12345",
+		URL:         server.URL,
+	}
+
+	searchRequest := SearchRequest{
+		Query:      "cillum",
+		Limit:      10,
+		Offset:     0,
+		OrderField: "Id",
+		OrderBy:    -1,
+	}
+
+	users, err := searchClient.FindUsers(searchRequest)
+	if err != nil || users == nil {
+		fmt.Errorf("Error")
+	}
+}
+
+func startSearchServer() *httptest.Server {
+	http.HandleFunc("/search/", handler)
+
+	return httptest.NewServer(http.HandlerFunc(handler))
+	//http.ListenAndServe(":8080", nil)
+}
